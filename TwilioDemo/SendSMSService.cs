e9634c1f748047ba67676e596bc79de7b729fb26
Wilfred Morgan <wmorgan@3cloudsolutions.com>
2022-07-05 20:12:37 -0400

    Add SMS Service

---
+++ TwilioDemo/SendSMSService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Twilio;
+using Twilio.Exceptions;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+namespace TwilioDemo
+{
+    public class SendSMSService
+    {
+        private string _accountSid;
+        private string _authToken;
+
+
+        public SendSMSService(string accountSid, string authToken)
+        {
+            _accountSid = accountSid;
+            _authToken = authToken;
+        }
+
+        public async Task<MessageResource> RunSendSMSService(PhoneNumber toPhone, string messagingServiceSid, string messageBody)
+        {
+            TwilioClient.Init(_accountSid, _authToken);
+
+            try
+            {
+                var messageResponse = await SendMessageAsync(CreateMessage(toPhone, messagingServiceSid, messageBody));
+                Console.WriteLine(messageResponse.Sid);
+                return messageResponse;
+            }
+            catch (ApiException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine($"Twilio Error {e.Code} - {e.MoreInfo}");
+                throw;
+            }
+            
+        }
+
+        protected async Task<MessageResource> SendMessageAsync(CreateMessageOptions messageOptions)
+        {
+            return await MessageResource.CreateAsync(messageOptions);
+        }
+
+        protected CreateMessageOptions CreateMessage(PhoneNumber toPhone, string messagingServiceSid, string messageBody)
+        {
+            CreateMessageOptions messageOptions = new(toPhone)
+            {
+                MessagingServiceSid = messagingServiceSid,
+                Body = messageBody
+            };
+
+            return messageOptions;
+        }
+    }
+}
