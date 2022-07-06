94d180dbbf9f9d9fb065c27e9434754470eaf319
Wilfred Morgan <wmorgan@3cloudsolutions.com>
2022-07-05 20:12:47 -0400

    Implement SMSService

--- TwilioDemo/Program.cs
+++ TwilioDemo/Program.cs
@@ -6,62 +6,77 @@ using Twilio.Exceptions;
 using Twilio.Rest.Api.V2010.Account;
 using Twilio.Types;
 
-class Program
+namespace TwilioDemo
 {
-    static void Main(string[] args)
+    class Program
     {
-        var builder = new ConfigurationBuilder()
-            .AddJsonFile("local.settings.json")
-            .AddUserSecrets<Program>();
-        var configuration = builder.Build();
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("local.settings.json")
+                .AddUserSecrets<Program>();
+            var configuration = builder.Build();
 
-        Console.WriteLine("Enter SMS message");
-        string? smsMessage = Console.ReadLine();
+            SendSMSService smsService = new(
+                 configuration.GetSection("TWILIO_ACCOUNT_SID").Value,
+                 configuration.GetSection("TWILIO_AUTH_TOKEN").Value);
 
-        SendSMS(
-            configuration.GetSection("TWILIO_ACCOUNT_SID").Value,
-            configuration.GetSection("TWILIO_AUTH_TOKEN").Value,
-            configuration.GetSection("Phone:From").Value,
-            configuration.GetSection("Phone:To").Value,
-            smsMessage
-            ).Wait();
+            Console.WriteLine("Enter SMS message");
+            string? smsMessage = Console.ReadLine();
 
-        Console.Write("Press any key to continue.");
-        Console.ReadKey();
+            smsService.RunSendSMSService(
+                configuration.GetSection("Phone:To").Value,
+                configuration.GetSection("MessagingServiceSid").Value,
+                smsMessage
+                ).Wait();
+
+            //SendSMS(
+            //    configuration.GetSection("TWILIO_ACCOUNT_SID").Value,
+            //    configuration.GetSection("TWILIO_AUTH_TOKEN").Value,
+            //    configuration.GetSection("Phone:From").Value,
+            //    configuration.GetSection("Phone:To").Value,
+            //    smsMessage
+            //    ).Wait();
+
+            Console.Write("Press any key to continue.");
+            Console.ReadKey();
+        }
+
+        static async Task SendSMS(
+            string accountSid,
+            string authToken,
+            string fromPhone,
+            string toPhone,
+            string messageBody
+            )
+        {
+            Console.WriteLine($"TWILIO_ACCOUNT_SID: {accountSid}");
+            Console.WriteLine($"TWILIO_AUTH_TOKEN: {authToken}");
+            Console.WriteLine($"FROM PHONE: {fromPhone}");
+            Console.WriteLine($"TO PHONE: {toPhone}");
+            Console.WriteLine($"MESSAGE BODY: {messageBody}");
+
+            TwilioClient.Init(accountSid, authToken);
+
+            try
+            {
+                var message = await MessageResource.CreateAsync(
+                    body: messageBody,
+                    from: new PhoneNumber(fromPhone),
+                    to: new PhoneNumber(toPhone)
+                );
+
+                Console.WriteLine(message.Sid);
+            }
+            catch (ApiException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine($"Twilio Error {e.Code} - {e.MoreInfo}");
+            }
+        }
     }
 
-    static async Task SendSMS(
-        string accountSid,
-        string authToken,
-        string fromPhone,
-        string toPhone,
-        string messageBody
-        )
-    {
-        Console.WriteLine($"TWILIO_ACCOUNT_SID: {accountSid}");
-        Console.WriteLine($"TWILIO_AUTH_TOKEN: {authToken}");
-        Console.WriteLine($"FROM PHONE: {fromPhone}");
-        Console.WriteLine($"TO PHONE: {toPhone}");
-        Console.WriteLine($"MESSAGE BODY: {messageBody}");
-
-        TwilioClient.Init(accountSid, authToken);
-
-        try
-        {
-            var message = await MessageResource.CreateAsync(
-                body: messageBody,
-                from: new PhoneNumber(fromPhone),
-                to: new PhoneNumber(toPhone)
-            );
-
-            Console.WriteLine(message.Sid);
-        }
-        catch (ApiException e)
-        {
-            Console.WriteLine(e.Message);
-            Console.WriteLine($"Twilio Error {e.Code} - {e.MoreInfo}");
-        }
-    }
 }
+
 
 
